facccac1f4db290bf33db520389250c59c5fc8e0
OllenkA <shamko.olga2018@gmail.com>
2019-12-08 06:30:39 +0300

    Updates

---
+++ precache-manifest.95c4841bb0579b8f2384d0b0dd39179f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f960fb6b43b30c2b501a6abb9916ea",
+    "url": "/color-music/index.html"
+  },
+  {
+    "revision": "1f5333fe724895ff541d",
+    "url": "/color-music/static/css/main.c2790ac8.chunk.css"
+  },
+  {
+    "revision": "3da24a95e860f792d7ad",
+    "url": "/color-music/static/js/2.21b041e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/color-music/static/js/2.21b041e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f5333fe724895ff541d",
+    "url": "/color-music/static/js/main.68e39aa0.chunk.js"
+  },
+  {
+    "revision": "790131103513099f6b8e",
+    "url": "/color-music/static/js/runtime-main.32669821.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-music/precache-manifest.70b753fe6c14fb67a77cef8080d7e67c.js"
+  "/color-music/precache-manifest.95c4841bb0579b8f2384d0b0dd39179f.js"
 );
 
 self.addEventListener('message', (event) => {
